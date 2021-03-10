3b8ee3d8ea037223a752a4ae26b06ede31e298a3
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-03-09 16:53:10 -0800

    Finished Unit 2 Lesson D

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -9,20 +10,22 @@ public class SpawnManager : MonoBehaviour
     private float xPosRange = 13;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3.0f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            float randXPos = Random.Range(-xPosRange, xPosRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+            
 
-            Vector3 randPos = new Vector3(randXPos, 0, 23);
-            Instantiate(animalPrefabs[animalPrefabIndex], randPos,
-                   animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+    }
+    void SpawnRandomAnimal()
+    {
+        float randXPos = Random.Range(-xPosRange, xPosRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 randPos = new Vector3(randXPos, 0, 23);
+        Instantiate(animalPrefabs[animalPrefabIndex], randPos,
+               animalPrefabs[animalPrefabIndex].transform.rotation);
     }
 }
